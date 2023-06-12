2236d7150001b856ef2e8ae686c2c29af473ceae
LadyRoseA <l.alarcon079@mybvc.ca>
2023-06-12 10:56:53 -0600

    Commit1

--- ConnectFour/Program.cs
+++ ConnectFour/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace ConnectFour
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
